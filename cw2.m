db87027ab2601620c206af7f52671dc1a03ae5cf
wilqqq <32998531+wilqqq@users.noreply.github.com>
2019-11-06 22:59:25 +0100

    Add files via upload

--- cw2.m
+++ cw2.m
@@ -1,5 +1,6 @@
 % zad 1 - budowa generatora siatki i jej wizualizacja
 % oceniamy wzrokowo czy spełnia kryteria
+
 % zad 2 - jak wygląda interpolacja dowolnej funkcji na przygotowanej
 % przestrzeni - przyjąć funkcję 2 zmiennych (trygonometryczne - nie pasują 
 % klasowo dofunkcji kształtu poly).
@@ -9,30 +10,49 @@
 % za pomocą ginput urzytkownik wybiera punkt i dowie się jaka jest tam
 % wartość funkcji oraz interpolowana wartość funkcji w tym punkcie (wybrać
 % el skończony i na jego podstawie wyznaczyć wartość punktu)
-%% zad 1
-% losowo
+
+%% ad 1 - random
 pointsl = rand(100,2);
+vall = sin(sum(pointsl,2));
 dtl = delaunayTriangulation(pointsl);
 % plot(Xl,Yl,'o')
 figure(1)
+subplot(1,2,1)
+stem3(pointsl(:,1),pointsl(:,2),vall,'LineStyle','none');
+subplot(1,2,2)
 triplot(dtl,pointsl(:,1),pointsl(:,2))
-title('losowe')
-% regularnie
+title('Random')
+
+%% ad 1 - regular
 t = 0:0.1:1;
 pointsr=[];
 for i=1:length(t)
     pointsr = [pointsr ;[ones(length(t),1).*t(i) t']];
 end
+valr = sin(sum(pointsr,2));
 dtr = delaunayTriangulation(pointsr);
 figure(2)
+subplot(1,2,1)
+stem3(pointsr(:,1),pointsr(:,2),valr,'LineStyle','none');
+%surf/plot(pointsr(:,1),pointsr(:,2),valr);
+subplot(1,2,2)
 triplot(dtr, pointsr(:,1), pointsr(:,2))
-title('regularne');
-% semi-regularnie
+title('Regular');
+
+%% ad 1 - semi-regular
 pointssr=[];
 for i=1:length(t)
     pointssr = [pointssr ;[ones(length(t),1).*t(i) rand(length(t),1)]];
 end
+valsr = sin(sum(pointssr,2));
 dtsr = delaunayTriangulation(pointssr);
 figure(3)
+subplot(1,2,1)
+stem3(pointssr(:,1),pointssr(:,2),valsr,'LineStyle','none');
+subplot(1,2,2)
 triplot(dtsr, pointssr(:,1), pointssr(:,2))
-title('semi-regularne');
+title('Semi-regular');
+
+%% ad 2
+%pkt3 = [pointsr(dtsr.ConnectivityList(1,:),1), pointsr(dtsr.ConnectivityList(1,:),2)];
+%pkt3 = pointsr(dtsr.ConnectivityList(1,:),:);
---
+++ mes_tri.m
@@ -0,0 +1,56 @@
+classdef mes_tri
+    %MES_TRI it's a class for interpolating values for delaunay
+    %triangualted set of data
+    %tri - triangulation object
+    %X - matrix of known dependent variables
+    %y - vector of known independent variables
+    %Coeffs - matrix of coefficients for relevant triads
+    
+    properties (Access = private)
+        tri
+        X
+        y
+        Coeffs
+    end
+    
+    methods
+        function obj = mes_tri(tri_obj,X_values,y_values)
+            %MES_TRI Construct an instance of this class
+            %   tri_obj - Triangulation object
+            %   X_values - matrix of independent variables
+            %   y_values - vector of dependent variables
+            obj.tri = tri_obj;
+            obj.X = X_values;
+            obj.y = y_values;
+            obj.Coeffs = [];
+            for tr=obj.tri.ConnectivityList' %for each triad
+                obj.Coeffs = [
+                    obj.Coeffs; 
+                    multi_lin_inter(X(tr,:),y(tr))
+                    ];
+            end
+        end
+        
+        function output = get_coefficients(obj)
+            output = obj.Coeffs;
+        end
+        
+        function y_pred = predict(obj, X)
+            %PREDICT 
+            si = size(X);
+            p = ones(si(1),si(2)+1);
+            si = 1;
+            for i=X %matrix of variables
+                p(:,si) = i; 
+                si += 1;
+            end
+            %calculate
+            %1 - choose rigth triad
+            %2 - takeout relevant coeeficients
+            %3 - predict
+            %x - make it work with matrixes!
+            y+pred = 0;
+        end
+    end
+end
+
